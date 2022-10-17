50f6c26b3f8810378e82f03c5e5ff76d28906549
claralv <lagunas.informatica@gmail.com>
2022-10-17 12:03:26 +0200

    Version 1.0

---
+++ app/src/main/java/com/example/parques/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.parques
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.parques.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val TAG="info"
+        binding.btnGuardar.setOnClickListener {
+            //Para los checkbox
+            if (binding.zonamascotas.isChecked)
+                Log.d(TAG," Las actvidades del parque son: ${binding.zonamascotas.text}")
+
+            //Para el spinner:
+            Log.d(TAG,"La hora de apertura es ${binding.SpinnerOpeningTime.selectedItem}")
+        }
+
+
+    }
+}
